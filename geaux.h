792fed4b3cd761101651426d9018c4f41a261b5b
Michael Valentino Ochoa <40745550+mochoabuilds@users.noreply.github.com>
2022-06-18 20:42:19 -0500

    Update geaux.h

--- geaux.h
+++ geaux.h
@@ -1,4 +1,4 @@
-// GeauxCard permits withdrawals only (eventually linked to autonomous checkout)
+// note: withdrawals only
 
 #ifndef GEAUX_H
 #define GEAUX_H
@@ -10,46 +10,45 @@ public:
     // Modifiers
     int cash(double amount);
         // amount is positive
-        // deducts amount from card
+        // deducts amount from tickets
         // returns 1 for yes and 0 for no
     void operator += (double incrementAmount);
-        // throttles cash supply
-        // $ - small re-up fee for this service
+        // throttles ticket supply
         // incrementAmount is positive
-        // increases card's worth if amount is greater than re-up fee
+        // increases tickets worth if amount is greater than 
     void operator= (const GeauxCard & rhs); // assignment operator
-        // turns object enacting method into copy of rhs/right hand side
+        // turns object enacting method into copy of rhs 
 
     // Accessors
     double retrieveBalance(void);
-        // returns remaining balance on GeauxCard
+        // returns remaining balance of tickets
     static double outstandingDebt(void);
-        // returns total of all balances on existing cards
+        // returns total of all balances on existing tickets
     double startUpFee(void);
-        // returns small re-up fee for making new GeauxCard
+        // returns small re-up fee for making bag for tickets
     double incrementFee(void);
-        // return fee for adding funds to existing GeauxCard
+        // nominal return fee for adding funds to existing GeauxCard
 
     // Conversion Members
     operator Account(void);
-        // type cast from a GeauxCard object to an Account object
+        // type cast from a ticket object to an Account object
 
     // Constructors
     GeauxCard(double purchasePrice);
         // purchasePrice > re-up fee
-        // creates a GeauxCard with some of purchasePrice used for initialization fees
+        // creates tickets with some of purchasePrice used for initialization fees
     GeauxCard(Account anExistingAccount); // conversion constructor
         // type casts from an Account object to a GeauxCard object
     GeauxCard(const GeauxCard & anExistingCard);  // copy constructor
     GeauxCard(void); // default constructor
-        // creates a GeauxCard with no money
+        // creates bag with no tickets
     ~GeauxCard(void); // destructor
         // destroys card
         // updates outstanding total debt on existing cards
 
 private:
-    double balance; // cash value of card
-    static double outstandingFunds;  // total debt on all cards
+    double balance; // ticket value of bag
+    static double outstandingFunds;  // total debt on all bags
     //
 };
 #endif
